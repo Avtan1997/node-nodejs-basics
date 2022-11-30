fcdbaafb894a37b59f481b545ca7c744108d7420
Avtandil Tursunbekov <nice.avtan@gmail.com>
2022-11-30 21:22:19 +0600

    feat: add read

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
-const copy = async () => {
-    // Write your code here 
-};
+import {access, mkdir, copyFile, readdir} from 'node:fs/promises';
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const copy = async () => {
+  const source = __dirname + '/files'
+  const destination = __dirname + '/files_copy'
+  const existMsg = 'FS operation failed'
+
+  try {
+    await mkdir(destination);
+    const dirFiles = await readdir(source)
+    await Promise.all(dirFiles.map(i => copyFile (`${source}/${i}`, `${destination}/${i}`)))
+    console.log('source.txt was copied to destination.txt');
+  } catch (err){
+    throw new Error(existMsg);
+  }
+}
 copy();
